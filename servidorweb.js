a6454b9f1069150d14a7c93121b18413898f716b
Reyespilar56 <eduardorgonzalez55@gmail.com>
2024-11-01 16:46:52 -0600

    serrvidor

--- servidorweb.js
+++ servidorweb.js
@@ -27,6 +27,6 @@ app.post('/send-email', (req, res) => {
 });
 
 // Inicia el servidor
-app.listen(3000, () => {
-    console.log('Servidor corriendo en http://localhost:3000');
+app.listen(3010, () => {
+    console.log('Servidor corriendo en http://localhost:3010');
 });
